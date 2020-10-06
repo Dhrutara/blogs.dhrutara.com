1302fb7bb543aaa1da6a0d3f13bba1c51fcc1ebd
Ramesh Kanjinghat <RameshK@kaplantrucking.com>
2020-10-06 15:57:38 -0400

    images were missing extension for icons in manifest.json

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
     exportPathMap: async function () {
         const paths = {
-            '/': { page: '/' }
+            '/': { page: '/' },
+            'about': { page: '/about' }
         };
         return paths;
     },
