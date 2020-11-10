f71453ead002966fefbfb6bd131b168b9a290b0d
Ramesh Kanjinghat <ramesh.kanjinghat@hotmail.com>
2020-11-09 20:37:24 -0500

    Updated about

--- pages/about.tsx
+++ pages/about.tsx
@@ -16,7 +16,7 @@ export default function About() {
                 I live in Ohio, USA with my wife and 2 adorable kids.
             </section>
             <section className={utilStyles.headingMd}>
-                I can write & speak in Malayalam, English, Hindi, Telugu and C#, Javascript, jsx, sql, html. I use Azure & Azure Devops when I have to take to clouds. I am agile when it comes to safeguarding information; distributed, centralized, clustered and tabularized.
+                I can write & speak in Malayalam, English, Hindi, Telugu and C#, Javascript, jsx, sql, html. I use Azure & Azure Devops when I have to take to clouds. I am agile when I sprint.
             </section>
             <section className={utilStyles.headingSm}>
                 Dotnet (Framework & Core), C#, javascript, ASP.Net (web forms, MVC & core), React, Azure, Azure Devops, sql, html Git, npm, Software architecture & design, vendor management and leading teams.
